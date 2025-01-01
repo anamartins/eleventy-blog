93cd535807f92a1aa55af8e731ae7fbb3b9200bf
Ana Martins <550368+anamartins@users.noreply.github.com>
2025-01-01 20:59:16 +0100

    add date in home and posts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
-
+import { DateTime } from 'luxon'
 
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./blog/css/style.css");
@@ -23,6 +23,20 @@ export default function(eleventyConfig) {
         }
       }
     });
-    eleventyConfig.setTemplateFormats("md,html,css");
+
+    eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+      // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+      return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+    });
+
+    eleventyConfig.addFilter("postDate", (dateObj, format, zone) => {
+      // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+      return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd/LL/yyyy");
+    });
+  
+    eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+      // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+      return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat('yyyy-LL-dd');
+    });
   };
   
